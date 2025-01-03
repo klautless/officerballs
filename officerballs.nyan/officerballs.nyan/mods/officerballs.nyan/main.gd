25a554601fc98ff696d6a175f76ba2411c1a7a32
klautless <86319608+klautless@users.noreply.github.com>
2025-01-03 00:23:39 -0500

    Update main.gd

--- officerballs.nyan/officerballs.nyan/mods/officerballs.nyan/main.gd
+++ officerballs.nyan/officerballs.nyan/mods/officerballs.nyan/main.gd
@@ -12,21 +12,23 @@ func _ready(): pass
 
 func _process(delta):
 	
+	if Network.PLAYING_OFFLINE or Network.STEAM_LOBBY_ID <= 0:
+		loadedin = false
+		colorspin = false
+		plactor = null
+		return
+	
 	if timecheck > 0:
 		timecheck -= 1
 	elif timecheck <= 0:
 		timecheck = 60
-		if Network.PLAYING_OFFLINE or Network.STEAM_LOBBY_ID <= 0:
-			loadedin = false
-			colorspin = false
-			plactor = null
-			return
 		for actor in get_tree().get_nodes_in_group("controlled_player"):
 			if not is_instance_valid(actor): return
 			else:
 				if not loadedin:
 					plactor = actor
 					loadedin = true
+					
 	if loadedin:
 		_get_input()
 		if colorspin:
