d1b6ac4fae6561f7ddffe9878fdd1405af1c14e3
klautless <86319608+klautless@users.noreply.github.com>
2025-01-02 23:20:58 -0500

    Add files via upload

--- officerballs.musicaura/officerballs.musicaura/mods/officerballs.musicaura/main.gd
+++ officerballs.musicaura/officerballs.musicaura/mods/officerballs.musicaura/main.gd
@@ -1,1 +1,30 @@
+extends Node
 
+var timecheck = 0
+var loadedin = false
+var plactor
+
+func _ready(): pass
+
+func _process(delta):
+	
+	if timecheck > 0:
+		timecheck -= 1
+	elif timecheck <= 0:
+		timecheck = 60
+		if Network.PLAYING_OFFLINE or Network.STEAM_LOBBY_ID <= 0:
+			loadedin = false
+			plactor = null
+			return
+		for actor in get_tree().get_nodes_in_group("controlled_player"):
+			if not is_instance_valid(actor): return
+			else:
+				if not loadedin:
+					plactor = actor
+					loadedin = true
+	if loadedin:
+		if randf() < 0.06:
+			var a = (randf() - 0.5) * 1.2
+			var b = randf() - 0.5
+			var c = (randf() - 0.5) * 1.2
+			plactor._sync_particle("music",Vector3(a,b,c))
