325e5c32dd4f5def2e3a5f786841da08c52bc934
klautless <86319608+klautless@users.noreply.github.com>
2025-01-07 09:20:13 -0500

    Add files via upload

--- officerballs.autoalltags/officerballs.autoalltags/Mod.cs
+++ officerballs.autoalltags/officerballs.autoalltags/Mod.cs
@@ -1,1 +1,17 @@
+﻿using GDWeave;
 
+namespace AutoAllTags;
+
+public class Mod : IMod {
+    public Config Config;
+
+    public Mod(IModInterface modInterface) {
+        this.Config = modInterface.ReadConfig<Config>();
+        modInterface.RegisterScriptMod(new AutoTagMod());
+        modInterface.Logger.Information("Hello, world!");
+    }
+
+    public void Dispose() {
+        // Cleanup anything you do here
+    }
+}
