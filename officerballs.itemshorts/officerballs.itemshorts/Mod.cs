bd4baa407170cb549d2333ffc19c465d770c500f
klautless <86319608+klautless@users.noreply.github.com>
2025-01-13 13:02:29 -0500

    Add files via upload

--- officerballs.itemshorts/officerballs.itemshorts/Mod.cs
+++ officerballs.itemshorts/officerballs.itemshorts/Mod.cs
@@ -1,1 +1,17 @@
+﻿using GDWeave;
 
+namespace ItemShorts;
+
+public class Mod : IMod {
+    public Config Config;
+
+    public Mod(IModInterface modInterface) {
+        this.Config = modInterface.ReadConfig<Config>();
+        modInterface.RegisterScriptMod(new ItemShortsMod());
+        modInterface.Logger.Information("Hello, world!");
+    }
+
+    public void Dispose() {
+        // Cleanup anything you do here
+    }
+}
