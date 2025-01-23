752f8aa6c4077acfc8f8bbe0f3334f699599d09d
klautless <86319608+klautless@users.noreply.github.com>
2025-01-23 13:08:47 -0500

    Create main.gd

---
+++ officerballs.bufflib/godot/mods/officerballs.bufflib/main.gd
@@ -0,0 +1,36 @@
+extends Node
+
+const MOD_ID = "officerballs.bufflib"
+
+onready var TackleBox := get_node_or_null("/root/TackleBox")
+
+var config: Dictionary
+var default_config: Dictionary = {
+	"RandomBuffsFromFishing": false
+}
+
+func _ready():
+	config = default_config
+	if TackleBox != null:
+		TackleBox.connect("mod_config_updated", self, "_on_config_update")
+		_init_config()
+	pass
+
+func _init_config() -> void:
+	var saved_config = TackleBox.get_mod_config(MOD_ID)
+
+	for key in default_config.keys():
+		if not saved_config[key]: # If the config property isn't saved...
+			saved_config[key] = default_config[key] # Set it to the default
+	
+	config = saved_config
+	TackleBox.set_mod_config(MOD_ID, config) # Save it to a config file!
+
+func _on_config_update(mod_id: String, new_config: Dictionary) -> void:
+	if mod_id != MOD_ID:
+		return
+
+	if config.hash() == new_config.hash():
+		return
+
+	self.config = new_config
